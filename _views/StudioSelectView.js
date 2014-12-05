2d735d961d556ea10fb278a6042c3f3f1f0cf10e
Sean <slevy@mediasignage.com>
2014-12-04 17:41:29 -0800

    fixed so studio selection is with hover

--- _views/StudioSelectView.js
+++ _views/StudioSelectView.js
@@ -21,28 +21,43 @@ define(['jquery', 'backbone', 'bootbox'], function ($, Backbone, bootbox) {
                 Backbone.comBroker.getService(Backbone.SERVICES.LAYOUT_ROUTER).navigate('unauthenticated', {trigger: true});
             });
             self._listenStudioSelection();
-            self._listenStudioFocus();
         },
 
         _checkCompatibility: function () {
             if (BB.APPS_SUPPORT == BB.CONSTS.OS_MOBILE)
-                return 0
+                return 0;
             return 1;
         },
 
-        _listenStudioFocus: function () {
+        _listenStudioSelection: function () {
             var self = this;
-            $('div.product-chooser').find('div.product-chooser-item').on('click', function (e) {
-                if ($(this).attr('name') == 'StudioPro' && self._checkCompatibility() == 0) {
-                    bootbox.alert($(Elements.MSG_BOOTBOX_NO_PRO_ON_MOBILE).text());
-                    return;
-                }
+            var $elem = $('div.product-chooser').find('div.product-chooser-item');
+            $elem.on('mouseover', function (e) {
                 $('div.product-chooser-item').removeClass('selected');
                 $(this).addClass('selected');
-                $(this).find('input[type="radio"]').prop("checked", true);
+                $('input[type="radio"]').prop("checked", true);
+            });
+            $elem.on('click', function (e) {
+                switch ($(this).attr('name')){
+                    case 'StudioLite': {
+                        self.m_selectedStudioType = 'StudioLite';
+                        Backbone.comBroker.getService(Backbone.SERVICES.LAYOUT_ROUTER).navigate('selectStudioLite', {trigger: true});
+                        break;
+                    }
+                    case 'StudioPro': {
+                        if (self._checkCompatibility() == 0) {
+                            bootbox.alert($(Elements.MSG_BOOTBOX_NO_PRO_ON_MOBILE).text());
+                            return;
+                        }
+                        self.m_selectedStudioType = 'StudioPro';
+                        Backbone.comBroker.getService(Backbone.SERVICES.LAYOUT_ROUTER).navigate('selectStudioPro', {trigger: true});
+                        break;
+                    }
+                }
             });
         },
 
+        /*
         _listenStudioSelection: function () {
             var self = this;
             $(Elements.STUDIO_LITE_SELECTION).on('click', function (e) {
@@ -57,6 +72,7 @@ define(['jquery', 'backbone', 'bootbox'], function ($, Backbone, bootbox) {
                 Backbone.comBroker.getService(Backbone.SERVICES.LAYOUT_ROUTER).navigate('selectStudioPro', {trigger: true});
             });
         },
+        */
 
         showAccordionStudioPlayer: function (i_id) {
             var self = this;
