5b8d4571f07660ef897312b76f6f5a1636e9f584
root <born2net@gmail.com>
2017-04-24 08:40:51 -0700

    upd

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ define(['underscore', 'jquery', 'backbone', 'bootstrap', 'backbone.controller', 
 
         // app init
         initialize: function () {
-            console.log('msGetStarted version 2.80');
+            console.log('msGetStarted version 2.90');
             window.BB = Backbone;
             BB.globs = {};
             BB.SERVICES = {};
